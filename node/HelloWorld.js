623e11ef89973de1141a5bfb1ea60e377c5ad0fb
Teemwu <2580144317@qq.com>
2017-01-14 19:10:55 +0800

    node test.

---
+++ node/HelloWorld.js
@@ -0,0 +1,18 @@
+/**
+ * Created by xiangsheng on 2017-1-5.
+ * GitHub: https://github.com/Teemwu
+ */
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+res.setHeader('Content-Type', 'text/plain');
+res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
